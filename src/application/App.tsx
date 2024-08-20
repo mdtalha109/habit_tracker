2456d200d08b31688903fc382f097bee84f2e8f0
mdtalha109 <mdtalha1041@gmail.com>
2024-08-20 20:48:21 +0530

    feat: routing for home page

--- src/application/App.tsx
+++ src/application/App.tsx
@@ -6,28 +6,41 @@ import LoginPage from 'presentation/pages/auth/login'
 import { Toaster } from 'react-hot-toast'
 import PrivateRoute from './routes/privateRoutes'
 import HomePage from 'presentation/pages/home'
+import MainLayout from 'presentation/components/layout/mainLayout'
+import PublicRoute from './routes/publicRoutes'
 
 function App() {
 
   return (
     <>
     <Routes>
-      <Route path="/auth">
+      <Route path="/auth" element={<PublicRoute/>}>
           <Route path='sign-up' element={<RegisterPage/>}/>
           <Route path='login' element={<LoginPage/>}/>
       </Route>
 
       <Route path="/" element={<PrivateRoute/>}>
-      <Route path='' element={<HomePage/>}/>
-      </Route>
+        <Route path='/' element={<MainLayout/>}>
+          <Route path='' element={<HomePage/>}/>
+        </Route>
+        
+        </Route>
     </Routes>
 
     <Toaster
       toastOptions={{
         style: {
-          background: '#363636',
-          color: '#fff',
-        }
+          background: 'white',
+          color: '#7b0e7f',
+          padding: '10px',
+          border: '2px solid #7b0e7f',
+          boxShadow: '0px 0px 2px 0px #7b0e7f'
+
+        },
+        iconTheme: {
+          primary: '#7b0e7f',
+          secondary: '#FFFAEE',
+        },
       }}
      />
     </>
