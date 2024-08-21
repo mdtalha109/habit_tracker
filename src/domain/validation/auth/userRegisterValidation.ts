fe381d9adc691e8dc0a15048f1114971a223e119
mdtalha109 <mdtalha1041@gmail.com>
2024-08-21 10:35:58 +0530

    feat: validate password length

--- src/domain/validation/auth/userRegisterValidation.ts
+++ src/domain/validation/auth/userRegisterValidation.ts
@@ -4,5 +4,7 @@ import * as Yup from 'yup';
 export const registerValidationSchema = (t:(key: string, options?: Record<string, any>) => string) => Yup.object({
   username: Yup.string().required(t('auth.required', { required_entity: 'Name' })),
   email: Yup.string().email(t('auth.invalid_email')).required(t('auth.required', { required_entity: 'Email' })),
-  password: Yup.string().required(t('auth.required', { required_entity: 'Password' })),
+  password: Yup.string()
+    .required(t('auth.required', { required_entity: 'Password' }))
+    .min(6, t('auth.min_length', { required_entity: 'Password', min: 6 }))
 });
